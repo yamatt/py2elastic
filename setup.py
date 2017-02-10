6b621ad5fc3f6363c5c490ce8d0a021b530d9973
Matt Copperwaite <mattcopp@gmail.com>
2017-02-10 14:21:25 +0000

    Made description readable in the english language

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='py2elastic',
     version='0.1',
-    description='Take import file (csv) and put it in ElasticSearch',
+    description='Import a file (csv) and put it in ElasticSearch',
     log_description=open("README.md","r").read()
     author='Matt Copperwaite',
     author_email='matt@copperwaite.net',
